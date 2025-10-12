224fc50dd94588c0890007aa7c5ed48e9d96e847
Blowspear <43101439+kman0001@users.noreply.github.com>
2025-10-13 02:11:02 +0900

    Update cache.py

--- core/cache.py
+++ core/cache.py
@@ -1,25 +1,29 @@
 import json
 import threading
 from pathlib import Path
-from core.config import CACHE_FILE
 import logging
 
-cache = {}
-cache_modified = False
 cache_lock = threading.Lock()
+cache_modified = False
+cache = {}
 
-if CACHE_FILE.exists():
-    with CACHE_FILE.open("r", encoding="utf-8") as f:
-        cache = json.load(f)
+def init_cache(cache_file: Path):
+    global cache
+    if cache_file.exists():
+        with cache_file.open("r", encoding="utf-8") as f:
+            cache = json.load(f)
+    else:
+        cache = {}
+    return cache
 
-def save_cache():
+def save_cache(cache_file: Path):
     global cache_modified
     with cache_lock:
         if cache_modified:
-            CACHE_FILE.parent.mkdir(parents=True, exist_ok=True)
-            with CACHE_FILE.open("w", encoding="utf-8") as f:
+            cache_file.parent.mkdir(parents=True, exist_ok=True)
+            with cache_file.open("w", encoding="utf-8") as f:
                 json.dump(cache, f, indent=2, ensure_ascii=False)
-            logging.info(f"[CACHE] Saved to {CACHE_FILE}, {len(cache)} entries")
+            logging.info(f"[CACHE] Saved to {cache_file}, {len(cache)} entries")
             cache_modified = False
 
 def update_cache(video_path, ratingKey=None, nfo_hash=None):
@@ -33,11 +37,13 @@ def update_cache(video_path, ratingKey=None, nfo_hash=None):
             current["nfo_hash"] = nfo_hash
         cache[path] = current
         cache_modified = True
+        logging.debug(f"[CACHE] update_cache: {path} => {current}")
 
 def remove_from_cache(video_path):
     global cache_modified
     path = str(video_path)
     with cache_lock:
         if path in cache:
-            cache.pop(path, None)
+            cache.pop(path)
             cache_modified = True
+            logging.debug(f"[CACHE] remove_from_cache: {path}")
